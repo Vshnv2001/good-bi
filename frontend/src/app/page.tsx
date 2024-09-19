a29a0210e985ffa079420eac802c7cbe464ac05c
Lester Ong <lesterong776@gmail.com>
2024-09-19 18:12:59 +0800

    Add product section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -58,7 +58,29 @@ export default function Home() {
               </Button>
             </div>
           </div>
-          <AnimatedLogoCloud />
+          <AnimatedLogoCloud/>
+          <div className="bg-gray-100 rounded-2xl py-32">
+            <div className="mx-auto text-center px-4 w-full max-w-2xl">
+              <span className="text-primary-500" id="product">Product</span>
+              <h2 className="font-bold text-4xl mt-2 sm:text-5xl">
+                The ultimate <span className="text-primary-500">AI business intelligence</span> tool.
+              </h2>
+            </div>
+            <div className="grid grid-cols-1 max-w-5xl mx-auto gap-4 mt-6 px-8 md:grid-cols-3">
+              <div className="bg-white rounded-xl p-5">
+                <h3 className="text-lg font-bold">Effortless insights</h3>
+                <p className="leading-5">No knowledge in business intelligence required.</p>
+              </div>
+              <div className="bg-white rounded-xl p-5">
+                <h3 className="text-lg font-bold">Works anywhere</h3>
+                <p className="leading-5">On any device, from mobile phones to desktops.</p>
+              </div>
+              <div className="bg-white rounded-xl p-5">
+                <h3 className="text-lg font-bold">Designed for speed</h3>
+                <p className="leading-5">Generate key performance indicators in just minutes.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
