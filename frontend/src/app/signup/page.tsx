9614d90e261c338778d170e39b6fab7777e361fc
Lester Ong <lesterong776@gmail.com>
2024-09-20 23:40:00 +0800

    Add sign up and login pages

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,122 @@
+"use client"
+
+import Link from "next/link";
+import {Form, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
+import {LucideChevronLeft} from "lucide-react";
+
+const FormSchema = z.object({
+  name: z.string().min(1, { message: "Please enter a name." }),
+  email: z
+    .string()
+    .min(1, { message: "Please enter an email." })
+    .email({ message: "Invalid email." }),
+  password: z
+    .string()
+    .min(1, { message: "Please enter a password." }),
+})
+
+export default function Signup() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: ""
+    },
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(JSON.stringify(data, null, 2));
+  }
+
+  return (
+    <main className="bg-gray-100 h-screen grid w-full">
+      <Link
+        href="/"
+        className={`fixed top-4 left-4 sm:top-12 sm:left-12 inline-flex items-center gap-1 text-gray-500 
+                    transition-colors duration-100 hover:text-primary-700`
+        }
+      >
+        <LucideChevronLeft className="h-4.5 w-4.5" />
+        Back to GoodBI
+      </Link>
+      <div className="max-w-lg px-4 w-full place-self-center">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-4xl text-primary-500 font-extrabold w-fit mx-auto"
+        >
+          <Image src={`/icons/goodbi-logo.svg`} alt="GoodBI" className="h-12 w-10" width="28" height="28"/>
+          GoodBI
+          <span className="sr-only">GoodBI</span>
+        </Link>
+        <h1 className="font-bold text-3xl text-center mt-10">Join the ultimate AI business intelligence platform.</h1>
+        <p className="mt-4 text-center text-sm text-gray-500">
+          Already have an account? {" "}
+          <Link className="transition-colors duration-100 text-primary-700 hover:text-primary-500" href="/login">
+            Login
+          </Link>
+          .
+        </p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-sm mx-auto flex flex-col gap-y-5 w-full mt-8 mb-20">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="space-y-1">
+                  <FormLabel className="font-normal text-base text-gray-800">Name</FormLabel>
+                  <Input
+                    type="text"
+                    className="rounded-xl text-base border border-gray-200/70 bg-white shadow-none"
+                    {...field}
+                  />
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({field}) => (
+                <FormItem className="space-y-1">
+                  <FormLabel className="font-normal text-base text-gray-800">Email</FormLabel>
+                  <Input
+                    type="text"
+                    className="rounded-xl text-base border border-gray-200/70 bg-white shadow-none"
+                    {...field}
+                  />
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({field}) => (
+                <FormItem className="space-y-1">
+                  <FormLabel className="font-normal text-base text-gray-800">Password</FormLabel>
+                  <Input
+                    type="password"
+                    className="rounded-xl text-base border border-gray-200/70 bg-white shadow-none"
+                    {...field}
+                  />
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <Button type="submit"
+                    className="text-base text-white rounded-xl bg-primary-700 px-4 py-2.5 shadow-none hover:bg-primary-600">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </main>
+  )
+}
