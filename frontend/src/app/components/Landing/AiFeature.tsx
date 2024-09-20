b80f9a90da2679fd5d16ae44615e72b21307c8d7
Lester Ong <lesterong776@gmail.com>
2024-09-20 14:54:48 +0800

    Fix errors for HTML property

--- frontend/src/app/components/Landing/AiFeature.tsx
+++ frontend/src/app/components/Landing/AiFeature.tsx
@@ -114,17 +114,17 @@ export function AiFeature({
 const Icons = {
   csv: () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className="stroke-green-700">
-      <path fill="none" stroke="" stroke-linecap="round" stroke-linejoin="round"
+      <path fill="none" stroke="" strokeLinecap="round" strokeLinejoin="round"
             d="M1.5 3.5c0-.54.48-1 1.08-1H6.5l1.54 1h5.38c.6 0 1.08.44 1.08.98l-.09 9.04c0 .54-.48.98-1.08.98H2.58c-.6 0-1.08-.44-1.08-.98zm2 4v4m3-4v4m3-4v4m3-4v4m-9 0h9m-9-2h9m-9-2h9"/>
     </svg>
   ),
   excel: () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-      <path fill="#20744a" fill-rule="evenodd"
+      <path fill="#20744a" fillRule="evenodd"
             d="M28.781 4.405h-10.13V2.018L2 4.588v22.527l16.651 2.868v-3.538h10.13A1.16 1.16 0 0 0 30 25.349V5.5a1.16 1.16 0 0 0-1.219-1.095m.16 21.126H18.617l-.017-1.889h2.487v-2.2h-2.506l-.012-1.3h2.518v-2.2H18.55l-.012-1.3h2.549v-2.2H18.53v-1.3h2.557v-2.2H18.53v-1.3h2.557v-2.2H18.53v-2h10.411Z"/>
       <path fill="#20744a"
             d="M22.487 7.439h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323z"/>
-      <path fill="#fff" fill-rule="evenodd"
+      <path fill="#fff" fillRule="evenodd"
             d="m6.347 10.673l2.146-.123l1.349 3.709l1.594-3.862l2.146-.123l-2.606 5.266l2.606 5.279l-2.269-.153l-1.532-4.024l-1.533 3.871l-2.085-.184l2.422-4.663z"/>
     </svg>
   ),
