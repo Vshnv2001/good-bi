ce0910554a4e7111be96ccf9eeb84875b3ea2658
Lester Ong <lesterong776@gmail.com>
2024-09-20 10:06:37 +0800

    Refactor into FeatureSection

---
+++ frontend/src/app/components/Landing/FeatureSection.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import {useState} from "react";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+import {LucideChartArea, LucidePencilRuler, LucideSparkles} from "lucide-react";
+import {AiFeature} from "@/app/components/Landing/AiFeature";
+import {ChartFeature} from "@/app/components/Landing/ChartFeature";
+import CustomisableFeature from "@/app/components/Landing/CustomisableFeature";
+
+const FeatureSection = () => {
+  return (
+    <div className="py-32 px-4 md:px-16">
+      <span className="text-primary-500" id="features">Features</span>
+      <h2 className="text-4xl mt-2 max-w-xl sm:text-5xl">
+        <span className="font-bold">Get better insights, faster.</span>
+        <br/>
+        Powerful analytics on metrics that matter.
+      </h2>
+      <FeatureToggle />
+    </div>
+  )
+}
+
+export default FeatureSection;
+
+const FeatureToggle = () => {
+  const [value, setValue] = useState('item-1')
+
+  const handleValueChange = (newValue: string) => {
+    if (!newValue) {
+      return;
+    }
+
+    setValue(newValue);
+  }
+
+  return (
+    <div className="flex h-full mt-12 items-center gap-12 md:mt-0 md:h-[600px]">
+      <Accordion className="w-full md:w-2/5" type="single" collapsible value={value} onValueChange={handleValueChange}>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+          <span className="flex items-center gap-1.5 text-lg font-bold">
+            <LucideSparkles className="h-5 w-5"/> AI-powered
+          </span>
+          </AccordionTrigger>
+          <AccordionContent className="text-base">
+            Instantly generate key insights from your data, using the latest large-language models from OpenAI.
+            <AiFeature className="block h-[368px] md:hidden md:h-[500px]"/>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+          <span className="flex items-center gap-1.5 text-lg font-bold">
+            <LucideChartArea className="h-5 w-5"/> Data visualisations
+          </span>
+          </AccordionTrigger>
+          <AccordionContent className="text-base">
+            Get real-time data visualisations from the click of a button.
+            <div className="block pt-6 md:hidden md:pt-0">
+              <ChartFeature/>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+          <span className="flex items-center gap-1.5 text-lg font-bold">
+            <LucidePencilRuler className="h-5 w-5"/> Fully customisable
+          </span>
+          </AccordionTrigger>
+          <AccordionContent className="text-base">
+            Place your insights where you want them to be. Resize and move them freely on your dashboard.
+            <div className="block pt-6 md:hidden md:pt-0">
+              <CustomisableFeature/>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <div className="flex-grow hidden md:block">
+        {value === 'item-1' && <AiFeature/>}
+        {value === 'item-2' && <ChartFeature/>}
+        {value === 'item-3' && <CustomisableFeature/>}
+      </div>
+    </div>
+  )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,23 +3,10 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import AnimatedLogoCloud from "@/app/components/Landing/AnimatedLogoCloud";
-import Meteors from "@/app/components/Landing/Meteors";
-import RetroGrid from "@/app/components/Landing/RetroGrid";
-import Globe from "@/app/components/Landing/Globe";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-import {useState} from "react";
-import {LucideSparkles, LucideChartArea, LucidePencilRuler} from "lucide-react";
-import {AiFeature} from "@/app/components/Landing/AiFeature";
-import {ChartFeature} from "@/app/components/Landing/ChartFeature";
-import CustomisableFeature from "@/app/components/Landing/CustomisableFeature";
 import {IntegrationIconCloud} from "@/app/components/Landing/IntegrationIconCloud";
 import LandingNavBar from "@/app/components/Landing/LandingNavBar";
 import ProductSection from "@/app/components/Landing/ProductSection";
+import FeatureSection from "@/app/components/Landing/FeatureSection";
 
 export default function Home() {
   return (
@@ -48,15 +35,7 @@ export default function Home() {
           </div>
           <AnimatedLogoCloud/>
           <ProductSection />
-          <div className="py-32 px-4 md:px-16">
-            <span className="text-primary-500" id="features">Features</span>
-            <h2 className="text-4xl mt-2 max-w-xl sm:text-5xl">
-              <span className="font-bold">Get better insights, faster.</span>
-              <br/>
-              Powerful analytics on metrics that matter.
-            </h2>
-            <FeatureSection/>
-          </div>
+          <FeatureSection />
         </div>
         <div className="bg-gray-100">
           <div className="px-4 py-16 max-w-7xl mx-auto">
@@ -137,65 +116,4 @@ export default function Home() {
       </main>
     </>
   );
-}
-
-const FeatureSection = () => {
-  const [value, setValue] = useState('item-1')
-
-  const handleValueChange = (newValue: string) => {
-    if (!newValue) {
-      return;
-    }
-
-    setValue(newValue);
-  }
-
-  return (
-    <div className="flex h-full mt-12 items-center gap-12 md:mt-0 md:h-[600px]">
-      <Accordion className="w-full md:w-2/5" type="single" collapsible value={value} onValueChange={handleValueChange}>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-          <span className="flex items-center gap-1.5 text-lg font-bold">
-            <LucideSparkles className="h-5 w-5"/> AI-powered
-          </span>
-          </AccordionTrigger>
-          <AccordionContent className="text-base">
-            Instantly generate key insights from your data, using the latest large-language models from OpenAI.
-            <AiFeature className="block h-[368px] md:hidden md:h-[500px]"/>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>
-          <span className="flex items-center gap-1.5 text-lg font-bold">
-            <LucideChartArea className="h-5 w-5"/> Data visualisations
-          </span>
-          </AccordionTrigger>
-          <AccordionContent className="text-base">
-            Get real-time data visualisations from the click of a button.
-            <div className="block pt-6 md:hidden md:pt-0">
-              <ChartFeature/>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>
-          <span className="flex items-center gap-1.5 text-lg font-bold">
-            <LucidePencilRuler className="h-5 w-5"/> Fully customisable
-          </span>
-          </AccordionTrigger>
-          <AccordionContent className="text-base">
-            Place your insights where you want them to be. Resize and move them freely on your dashboard.
-            <div className="block pt-6 md:hidden md:pt-0">
-              <CustomisableFeature/>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <div className="flex-grow hidden md:block">
-        {value === 'item-1' && <AiFeature/>}
-        {value === 'item-2' && <ChartFeature/>}
-        {value === 'item-3' && <CustomisableFeature/>}
-      </div>
-    </div>
-  )
 }
