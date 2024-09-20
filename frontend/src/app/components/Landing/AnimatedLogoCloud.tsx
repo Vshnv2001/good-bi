7905341b825a675767a1febfd242be353bd007b9
Lester Ong <lesterong776@gmail.com>
2024-09-20 14:47:59 +0800

    Update logos

--- frontend/src/app/components/Landing/AnimatedLogoCloud.tsx
+++ frontend/src/app/components/Landing/AnimatedLogoCloud.tsx
@@ -3,42 +3,37 @@ import Marquee from "@/app/components/Landing/Marquee";
 
 const logos = [
   {
-    name: 'Vercel',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881430/vercel_wordmark_dark_mhv8u8.svg',
+    name: 'Tinder',
+    url: '/logos/tinder.png'
   },
   {
-    name: 'Nextjs',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881475/nextjs_logo_dark_gfkf8m.svg',
+    name: 'OkCupid',
+    url: '/logos/okcupid.png'
   },
   {
-    name: 'Prime',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg',
+    name: 'Hinge',
+    url: '/logos/hinge.png'
   },
   {
-    name: 'Trustpilot',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tkfspxqmjflfllbuqxsi.svg',
+    name: 'Coffee Meets Bagel',
+    url: '/logos/cmb.png'
   },
   {
-    name: 'Webflow',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg',
-  },
-
-  {
-    name: 'Airbnb',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg',
+    name: 'Bumble',
+    url: '/logos/bumble.png'
   },
   {
-    name: 'Tina',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/afqhiygywyphuou6xtxc.svg',
+    name: 'cs3216',
+    url: '/logos/cs3216.png'
   },
   {
-    name: 'Stackoverflow',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/ts1j4mkooxqmscgptafa.svg',
+    name: 'InternTwine',
+    url: '/logos/interntwine.svg'
   },
   {
-    name: 'mistral',
-    url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg',
-  },
+    name: 'National University of Singapore',
+    url: '/logos/nus.png'
+  }
 ]
 
 const AnimatedLogoCloud = () => {
@@ -54,7 +49,7 @@ const AnimatedLogoCloud = () => {
           <Image
             key={key}
             src={logo.url}
-            className="h-auto w-28 px-2 brightness-0 dark:invert"
+            className="h-10 w-auto px-2 brightness-0 dark:invert"
             width="112"
             height="40"
             alt={`${logo.name}`}
