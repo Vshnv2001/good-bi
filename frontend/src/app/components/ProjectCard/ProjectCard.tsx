2020186e1a601128cd58fe6df983d62e3b09474f
Jian Ming Lok <jianminglok@hotmail.com>
2024-09-22 15:39:46 +0800

    Add ProjectCard component

---
+++ frontend/src/app/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+import { EllipsisVertical } from "lucide-react";
+
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardFooter
+} from "@/components/ui/card"
+
+import { format } from "date-fns"
+
+import { Project } from "@/app/types/Project"
+
+interface DashboardCardProps extends React.HTMLAttributes<HTMLDivElement> {
+    project: Project
+}
+
+const ProjectCard = React.forwardRef<
+    HTMLDivElement,
+    DashboardCardProps
+>(({ project, children, className, ...props }, ref) => {
+    return (
+        <Card ref={ref} className={cn("flex flex-col", className)} {...props}>
+            <CardHeader className="flex-row items-center justify-between">
+                <Link href={`/projects/${project.id}/dashboard`} className="flex-grow">
+                    <CardTitle>{project.name}</CardTitle>
+                </Link>
+                <EllipsisVertical className="text-gray-500 w-5 h-5" onClick={() => console.log("Test")} />
+            </CardHeader>
+            <Link href="/projects/1/dashboard">
+                <CardFooter>
+                    <p>Updated {format(project.lastUpdated, "dd MMM yyyy")}</p>
+                </CardFooter>
+            </Link>
+        </Card>
+    );
+});
+ProjectCard.displayName = "ProjectCard"
+
+export default ProjectCard;
