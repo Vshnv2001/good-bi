7b074710aa418e81dc71fc4243265ff36eac6aee
Jian Ming Lok <jianminglok@hotmail.com>
2024-09-22 15:41:04 +0800

    Update NavBar links

--- frontend/src/app/components/NavBar/NavBar.tsx
+++ frontend/src/app/components/NavBar/NavBar.tsx
@@ -37,10 +37,10 @@ const NavBar = () => {
                 <SheetContent side="left">
                     <nav className="grid gap-6 text-lg font-normal mt-8">
                         <Link
-                            href="/dashboard"
-                            className={`${currentPath === "/dashboard" ? "" : "text-muted-foreground"} hover:text-foreground`}
+                            href="/projects"
+                            className={`${currentPath === "/projects" ? "" : "text-muted-foreground"} hover:text-foreground`}
                         >
-                            Dashboards
+                            Projects
                         </Link>
                         <Link
                             href="/dataset"
@@ -61,10 +61,10 @@ const NavBar = () => {
             </Link>
             <nav className="hidden flex-col gap-6 text-base font-normal gap-6 md:flex md:w-full md:flex-row md:items-center md:justify-center">
                 <Link
-                    href="/dashboard"
-                    className={`${currentPath === "/dashboard" ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
+                    href="/projects"
+                    className={`${currentPath === "/projects" ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
                 >
-                    Dashboard
+                    Projects
                 </Link>
                 <Link
                     href="/dataset"
