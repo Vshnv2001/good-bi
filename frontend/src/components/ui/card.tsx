611e63ee136fc913ed3d8229bf8e2f134f92740b
Jian Ming Lok <jianminglok@hotmail.com>
2024-09-22 13:49:08 +0800

    Update styling for card footer

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -67,7 +67,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex items-center p-3 pt-0 text-gray-500", className)}
     {...props}
   />
 ))
