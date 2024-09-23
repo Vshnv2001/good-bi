6d3e00629e3d0d5de3aae42508fddeb85efc7b1f
Jian Ming Lok <jianminglok@hotmail.com>
2024-09-23 12:03:49 +0800

    Add dropdown menu for project card

--- frontend/src/app/components/ProjectCard/ProjectCard.tsx
+++ frontend/src/app/components/ProjectCard/ProjectCard.tsx
@@ -13,6 +13,14 @@ import {
     CardTitle,
     CardFooter
 } from "@/components/ui/card"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 import { format } from "date-fns"
 
@@ -32,7 +40,19 @@ const ProjectCard = React.forwardRef<
                 <Link href={`/projects/${project.id}/dashboard`} className="flex-grow">
                     <CardTitle>{project.name}</CardTitle>
                 </Link>
-                <EllipsisVertical className="text-gray-500 w-5 h-5" onClick={() => console.log("Test")} />
+                <div className="flex items-center md:ml-auto">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <EllipsisVertical className="text-gray-500 w-5 h-5" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <Link href={`/projects/${project.id}/update`}>
+                                <DropdownMenuItem>Edit</DropdownMenuItem>
+                            </Link>
+                            <DropdownMenuItem>Update</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </CardHeader>
             <Link href="/projects/1/dashboard">
                 <CardFooter>
