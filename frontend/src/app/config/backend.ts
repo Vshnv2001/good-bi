5d80dbfdd8636fce173b2436d05cd32ecddf0964
Vshnv2001 <mvaishnav.sg@gmail.com>
2024-09-23 11:13:38 +0800

    Add integration for supertokens

---
+++ frontend/src/app/config/backend.ts
@@ -0,0 +1,31 @@
+import SuperTokens from "supertokens-node";
+import EmailPasswordNode from 'supertokens-node/recipe/emailpassword'
+import SessionNode from 'supertokens-node/recipe/session'
+import { appInfo } from './appInfo'
+import { TypeInput } from "supertokens-node/types";
+
+export const backendConfig = (): TypeInput => {
+  return {
+    framework: "custom",
+    supertokens: {
+      // These are the connection details of the app you created on supertokens.com
+      connectionURI: process.env.SUPERTOKENS_CONNECTION_URI || "",
+      apiKey: process.env.SUPERTOKENS_API_KEY || "",
+    },
+    appInfo,
+    recipeList: [
+      EmailPasswordNode.init(),
+      SessionNode.init(),
+    ],
+    isInServerlessEnv: true,
+  }
+}
+
+let initialized = false;
+// This function is used in your APIs to make sure SuperTokens is initialised
+export function ensureSuperTokensInit() {
+  if (!initialized) {
+    SuperTokens.init(backendConfig());
+    initialized = true;
+  }
+}
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -9,6 +9,7 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import Image from "next/image";
 import {LucideChevronLeft} from "lucide-react";
+import { signIn } from "supertokens-web-js/recipe/emailpassword";
 
 const FormSchema = z.object({
   email: z
@@ -29,8 +30,46 @@ export default function Login() {
     resolver: zodResolver(FormSchema),
   })
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(JSON.stringify(data, null, 2));
+    try {
+      let response = await signIn({
+          formFields: [{
+              id: "email",
+              value: data.email
+          }, {
+              id: "password",
+              value: data.password
+          }]
+      })
+
+      if (response.status === "FIELD_ERROR") {
+          response.formFields.forEach(formField => {
+              if (formField.id === "email") {
+                  // Email validation failed (for example incorrect email syntax).
+                  window.alert(formField.error)
+              }
+          })
+      } else if (response.status === "WRONG_CREDENTIALS_ERROR") {
+          window.alert("Email password combination is incorrect.")
+      } else if (response.status === "SIGN_IN_NOT_ALLOWED") {
+          // the reason string is a user friendly message
+          // about what went wrong. It can also contain a support code which users
+          // can tell you so you know why their sign in was not allowed.
+          window.alert(response.reason)
+      } else {
+          // sign in successful. The session tokens are automatically handled by
+          // the frontend SDK.
+          window.location.href = "/dashboard"
+      }
+  } catch (err: any) {
+      if (err.isSuperTokensGeneralError === true) {
+          // this may be a custom error message sent from the API by you.
+          window.alert(err.message);
+      } else {
+          window.alert("Oops! Something went wrong.");
+        }
+    }
   }
 
   return (
