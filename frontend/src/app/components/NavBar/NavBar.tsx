fa4757e3676979c7438046193ebc7ea09d0f31af
Lester Ong <lesterong776@gmail.com>
2024-09-24 21:41:03 +0800

    Update navbar tabs

--- frontend/src/app/components/NavBar/NavBar.tsx
+++ frontend/src/app/components/NavBar/NavBar.tsx
@@ -1,105 +1,117 @@
 "use client";
 
-import { usePathname } from 'next/navigation'
+import {usePathname} from 'next/navigation'
 
 import Link from "next/link"
 import Image from 'next/image';
 
-import { Menu, CircleUser } from "lucide-react";
+import {Menu, CircleUser, Share, LogOut, HelpCircle} from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import {Button} from "@/components/ui/button"
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet"
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import Session from "supertokens-web-js/recipe/session";
 
 const NavBar = () => {
-    const currentPath = usePathname();
+  const currentPath = usePathname();
 
-    const signOut = async () => {
-        await Session.signOut();
-        window.location.href = "/" // TODO: Do we want to redirect to the login page?
-    }
+  const signOut = async () => {
+    await Session.signOut();
+    window.location.href = "/"
+  }
 
-    return (
-        <header className="sticky top-0 flex h-14 items-center gap-4 bg-background px-4 justify-between">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button
-                        variant="outline"
-                        size="icon"
-                        className="shrink-0 md:hidden"
-                    >
-                        <Menu className="h-5 w-5" />
-                        <span className="sr-only">Toggle navigation menu</span>
-                    </Button>
-                </SheetTrigger>
-                <SheetContent side="left">
-                    <nav className="grid gap-6 text-lg font-normal mt-8">
-                        <Link
-                            href="/projects"
-                            className={`${currentPath === "/projects" ? "" : "text-muted-foreground"} hover:text-foreground`}
-                        >
-                            Projects
-                        </Link>
-                        <Link
-                            href="/dataset"
-                            className={`${currentPath.includes("/dataset") ? "" : "text-muted-foreground"} hover:text-foreground`}
-                        >
-                            Datasets
-                        </Link>
-                    </nav>
-                </SheetContent>
-            </Sheet>
+  return (
+    <header className="sticky top-0 flex h-14 items-center gap-4 bg-background px-4 justify-between">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5"/>
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-normal mt-8">
             <Link
-                href="#"
-                className="flex items-center gap-2 text-2xl text-primary-500 font-extrabold md:flex"
+              href="/projects"
+              className={`${currentPath === "/projects" ? "" : "text-muted-foreground"} hover:text-foreground`}
             >
-                <Image src={`/icons/goodbi-logo.svg`} alt="GoodBI" width="32" height="32" />
-                 GoodBI
-                <span className="sr-only">GoodBI</span>
+              Projects
             </Link>
-            <nav className="hidden flex-col gap-6 text-base font-normal gap-6 md:flex md:w-full md:flex-row md:items-center md:justify-center">
-                <Link
-                    href="/projects"
-                    className={`${currentPath === "/projects" ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
-                >
-                    Projects
-                </Link>
-                <Link
-                    href="/dataset"
-                    className={`${currentPath.includes("/dataset") ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
-                >
-                    Dataset
-                </Link>
+            <Link
+              href="/dataset"
+              className={`${currentPath.includes("/dataset") ? "" : "text-muted-foreground"} hover:text-foreground`}
+            >
+              Datasets
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <Link
+        href="#"
+        className="flex items-center gap-2 text-2xl text-primary-500 font-extrabold md:flex"
+      >
+        <Image src={`/icons/goodbi-logo.svg`} alt="GoodBI" width="32" height="32"/>
+        GoodBI
+        <span className="sr-only">GoodBI</span>
+      </Link>
+      <nav
+        className="hidden flex-col gap-6 text-base font-normal gap-6 md:flex md:w-full md:flex-row md:items-center md:justify-center">
+        <Link
+          href="/projects"
+          className={`${currentPath === "/projects" ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/dataset"
+          className={`${currentPath.includes("/dataset") ? "text-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground`}
+        >
+          Dataset
+        </Link>
 
-            </nav>
-            <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="secondary" size="icon" className="rounded-full">
-                            <CircleUser className="h-5 w-5" />
-                            <span className="sr-only">Toggle user menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Settings</DropdownMenuItem>
-                        <DropdownMenuItem>Support</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={signOut}>Logout</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </header>
-    );
+      </nav>
+      <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5"/>
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator/>
+            <DropdownMenuItem>
+              <HelpCircle className="size-4 mr-1.5"/>
+              Need help? Email us.
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={signOut} asChild>
+              <a href="mailto:cs3216-staff@googlegroups.com">
+                <Share className="size-4 mr-1.5"/>
+                Share GoodBI
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator/>
+            <DropdownMenuItem onClick={signOut}>
+              <LogOut className="size-4 mr-1.5"/>
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
