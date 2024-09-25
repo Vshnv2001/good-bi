791913e122ed67768cc907960fd0fea3f224de62
Jian Ming Lok <jianminglok@hotmail.com>
2024-09-25 19:21:29 +0800

    Use correct env variable

--- frontend/src/app/config/appInfo.ts
+++ frontend/src/app/config/appInfo.ts
@@ -2,7 +2,7 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "goodbi",
-  websiteDomain: process.env.NEXT_PUBLIC_API_URL,
-  apiDomain: process.env.NEXT_PUBLIC_API_URL,
+  websiteDomain: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  apiDomain: process.env.NEXT_PUBLIC_FRONTEND_URL,
   apiBasePath: "/api/auth",
 }
