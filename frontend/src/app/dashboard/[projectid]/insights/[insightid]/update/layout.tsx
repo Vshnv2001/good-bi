04ab124ea05d788969a78573b4383357f631a1ba
Lester Ong <lesterong776@gmail.com>
2024-09-26 21:22:19 +0800

    Fix casing

--- frontend/src/app/dashboard/[projectid]/insights/[insightid]/update/layout.tsx
+++ frontend/src/app/dashboard/[projectid]/insights/[insightid]/update/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: `Edit Insight | ${process.env.APP_NAME}`,
+  title: `Edit insight | ${process.env.APP_NAME}`,
   description: `Edit insight on ${process.env.APP_NAME}.`
 }
 export default function ClientLayout({ children }: { children: ReactNode }) {
